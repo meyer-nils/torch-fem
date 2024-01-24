224a7b2ef064964bd4149538d8bb43a29f849765
Nils Meyer <nils.meyer@uni-a.de>
2024-01-24 22:24:56 +0100

    Add more tests.

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -14,3 +14,28 @@ def test_shell_notebook(tb):
 @testbook("examples/basic/truss.ipynb", execute=True)
 def test_truss_notebook(tb):
     tb.execute()
+
+
+@testbook("examples/basic/solid.ipynb", execute=True)
+def test_solid_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/optimization/topology_optimization.ipynb", execute=True)
+def test_topology_optimization_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/optimization/shape_optimization_planar.ipynb", execute=True)
+def test_shape_optimization_planar_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/optimization/orientation_optimization_planar.ipynb", execute=True)
+def test_orientation_optimization_planar_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/optimization/orientation_optimization_shell.ipynb", execute=True)
+def test_orientation_optimization_shell_notebook(tb):
+    tb.execute()
