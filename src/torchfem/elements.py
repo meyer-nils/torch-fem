81f10e27ba097808ee89f8dc264ef7fb4bb161ce
Nils Meyer <nils.meyer@uni-a.de>
2024-03-23 13:06:09 +0100

    Enable batch evaluation of shape functions.

--- src/torchfem/elements.py
+++ src/torchfem/elements.py
@@ -8,10 +8,10 @@ class Tria1:
         self.nodes = 3
 
     def N(self, xi: torch.Tensor) -> torch.Tensor:
-        N_1 = 1.0 - xi[0] - xi[1]
-        N_2 = xi[0]
-        N_3 = xi[1]
-        return torch.tensor([N_1, N_2, N_3])
+        N_1 = 1.0 - xi[..., 0] - xi[..., 1]
+        N_2 = xi[..., 0]
+        N_3 = xi[..., 1]
+        return torch.stack([N_1, N_2, N_3], dim=-1)
 
     def B(self, _: torch.Tensor) -> torch.Tensor:
         return torch.tensor([[-1.0, 1.0, 0.0], [-1.0, 0.0, 1.0]])
@@ -28,11 +28,11 @@ class Quad1:
         self.nodes = 4
 
     def N(self, xi: torch.Tensor) -> torch.Tensor:
-        N_1 = (1.0 - xi[0]) * (1.0 - xi[1])
-        N_2 = (1.0 + xi[0]) * (1.0 - xi[1])
-        N_3 = (1.0 + xi[0]) * (1.0 + xi[1])
-        N_4 = (1.0 - xi[0]) * (1.0 + xi[1])
-        return 0.25 * torch.tensor([N_1, N_2, N_3, N_4])
+        N_1 = (1.0 - xi[..., 0]) * (1.0 - xi[..., 1])
+        N_2 = (1.0 + xi[..., 0]) * (1.0 - xi[..., 1])
+        N_3 = (1.0 + xi[..., 0]) * (1.0 + xi[..., 1])
+        N_4 = (1.0 - xi[..., 0]) * (1.0 + xi[..., 1])
+        return 0.25 * torch.stack([N_1, N_2, N_3, N_4], dim=-1)
 
     def B(self, xi: torch.Tensor) -> torch.Tensor:
         return 0.25 * torch.tensor(
@@ -60,11 +60,11 @@ class Tetra1:
         self.nodes = 4
 
     def N(self, xi: torch.Tensor) -> torch.Tensor:
-        N_1 = 1.0 - xi[0] - xi[1] - xi[2]
-        N_2 = xi[0]
-        N_3 = xi[1]
-        N_4 = xi[2]
-        return torch.stack([N_1, N_2, N_3, N_4], dim=2)
+        N_1 = 1.0 - xi[..., 0] - xi[..., 1] - xi[..., 2]
+        N_2 = xi[..., 0]
+        N_3 = xi[..., 1]
+        N_4 = xi[..., 2]
+        return torch.stack([N_1, N_2, N_3, N_4], dim=-1)
 
     def B(self, _: torch.Tensor) -> torch.Tensor:
         return torch.tensor(
@@ -83,15 +83,15 @@ class Hexa1:
         self.nodes = 8
 
     def N(self, xi: torch.Tensor) -> torch.Tensor:
-        N_1 = (1.0 - xi[0]) * (1.0 - xi[1]) * (1.0 - xi[2])
-        N_2 = (1.0 + xi[0]) * (1.0 - xi[1]) * (1.0 - xi[2])
-        N_3 = (1.0 + xi[0]) * (1.0 + xi[1]) * (1.0 - xi[2])
-        N_4 = (1.0 - xi[0]) * (1.0 + xi[1]) * (1.0 - xi[2])
-        N_5 = (1.0 - xi[0]) * (1.0 - xi[1]) * (1.0 + xi[2])
-        N_6 = (1.0 + xi[0]) * (1.0 - xi[1]) * (1.0 + xi[2])
-        N_7 = (1.0 + xi[0]) * (1.0 + xi[1]) * (1.0 + xi[2])
-        N_8 = (1.0 - xi[0]) * (1.0 + xi[1]) * (1.0 + xi[2])
-        return 0.125 * torch.tensor([N_1, N_2, N_3, N_4, N_5, N_6, N_7, N_8])
+        N_1 = (1.0 - xi[..., 0]) * (1.0 - xi[..., 1]) * (1.0 - xi[..., 2])
+        N_2 = (1.0 + xi[..., 0]) * (1.0 - xi[..., 1]) * (1.0 - xi[..., 2])
+        N_3 = (1.0 + xi[..., 0]) * (1.0 + xi[..., 1]) * (1.0 - xi[..., 2])
+        N_4 = (1.0 - xi[..., 0]) * (1.0 + xi[..., 1]) * (1.0 - xi[..., 2])
+        N_5 = (1.0 - xi[..., 0]) * (1.0 - xi[..., 1]) * (1.0 + xi[..., 2])
+        N_6 = (1.0 + xi[..., 0]) * (1.0 - xi[..., 1]) * (1.0 + xi[..., 2])
+        N_7 = (1.0 + xi[..., 0]) * (1.0 + xi[..., 1]) * (1.0 + xi[..., 2])
+        N_8 = (1.0 - xi[..., 0]) * (1.0 + xi[..., 1]) * (1.0 + xi[..., 2])
+        return 0.125 * torch.stack([N_1, N_2, N_3, N_4, N_5, N_6, N_7, N_8], dim=-1)
 
     def B(self, xi: torch.Tensor) -> torch.Tensor:
         return 0.125 * torch.tensor(
