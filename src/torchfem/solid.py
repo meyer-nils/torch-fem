Fix solid stress computation with inelastic strains.

src/torchfem/solid.py
@@ -130,7 +130,7 @@ class Solid:
         f = f.reshape((-1, 3))
         return u, f
 
-    def compute_stress(self, u, xi=[0.0, 0.0]):
+    def compute_stress(self, u, xi=[0.0, 0.0, 0.0]):
         # Extract node positions of element
         nodes = self.nodes[self.elements, :]
 
@@ -146,8 +146,13 @@ class Solid:
         # Compute D
         D = self.D(B)
 
+        # Compute strain
+        epsilon = torch.einsum("...ij,...j->...i", D, disp)
+        if self.strains is not None:
+            epsilon -= self.strains
+
         # Compute stress
-        sigma = torch.einsum("...ij,...jk,...k->...i", self.C, D, disp)
+        sigma = torch.einsum("...ij,...j->...i", self.C, epsilon)
 
         # Return stress
         return sigma