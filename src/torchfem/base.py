253b70f1de5c9fb91ba2178a643e8756cb135f05
Nils Meyer <nils.meyer@uni-a.de>
2024-04-29 13:41:13 +0200

    Ensure sparse backward() for sparse_solve().

--- src/torchfem/base.py
+++ src/torchfem/base.py
@@ -5,25 +5,51 @@ from torch.autograd import Function
 
 
 class Solve(Function):
+    """
+    Inspired by
+    - https://blog.flaport.net/solving-sparse-linear-systems-in-pytorch.html
+    - https://github.com/cai4cai/torchsparsegradutils
+    """
+
     @staticmethod
     def forward(ctx, A, b):
+        # Check the input and make sure it is coalesced
         if A.ndim != 2 or (A.shape[0] != A.shape[1]):
             raise ValueError("A should be a square 2D matrix.")
         shape = A.size()
+        A = A.coalesce()
+
+        # Convert to numpy
         A_np = coo_matrix(
             (A._values(), (A._indices()[0], A._indices()[1])), shape=shape
         ).tocsr()
         b_np = b.data.numpy()
+
+        # Solve the system
         x_np = spsolve(A_np, b_np)
+
+        # Convert back to torch
         x = torch.tensor(x_np, requires_grad=True)
+
+        # Save the variables
         ctx.save_for_backward(A, b, x)
+
         return x
 
     @staticmethod
     def backward(ctx, grad):
+        # Access the saved variables
         A, b, x = ctx.saved_tensors
+
+        # Backprop rule: gradb = A^T @ grad
         gradb = Solve.apply(A.T, grad)
-        gradA = -gradb[:, None] * x[None, :]
+
+        # Backprop rule: gradA = -gradb @ x^T, sparse version
+        row = A.indices()[0, :]
+        col = A.indices()[1, :]
+        val = -gradb.index_select(0, row) * x.index_select(0, col)
+        gradA = torch.sparse_coo_tensor(torch.stack([row, col]), val, A.shape)
+
         return gradA, gradb
 
 
