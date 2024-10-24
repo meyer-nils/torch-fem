Incorporate external strain and fix stress computation.

src/torchfem/materials.py
@@ -4,21 +4,22 @@ import torch
 
 
 class Isotropic:
-    def __init__(self, E: Union[float, torch.Tensor], nu: Union[float, torch.Tensor]):
+    def __init__(
+        self,
+        E: Union[float, torch.Tensor],
+        nu: Union[float, torch.Tensor],
+        eps0: Union[float, torch.Tensor] = 0.0,
+    ):
         # Convert float inputs to tensors
-        if isinstance(E, float) and isinstance(nu, float):
+        if isinstance(E, float) and isinstance(nu, float) and isinstance(eps0, float):
             E = torch.tensor(E)
             nu = torch.tensor(nu)
+            eps0 = torch.tensor(eps0)
 
         # Store material properties
         self.E = E
         self.nu = nu
-
-        # We assume a vectorized material, if E is a tensor
-        if self.E.dim() == 0:
-            self.is_vectorized = False
-        else:
-            self.is_vectorized = True
+        self.eps0 = eps0
 
         # Lame parameters
         self.lbd = self.E * self.nu / ((1.0 + self.nu) * (1.0 - 2.0 * self.nu))
@@ -26,17 +27,12 @@ class Isotropic:
 
         # Stiffness tensor
         z = torch.zeros_like(self.E)
+        diag = self.lbd + 2.0 * self.G
         self.C = torch.stack(
             [
-                torch.stack(
-                    [self.lbd + 2.0 * self.G, self.lbd, self.lbd, z, z, z], dim=-1
-                ),
-                torch.stack(
-                    [self.lbd, self.lbd + 2.0 * self.G, self.lbd, z, z, z], dim=-1
-                ),
-                torch.stack(
-                    [self.lbd, self.lbd, self.lbd + 2.0 * self.G, z, z, z], dim=-1
-                ),
+                torch.stack([diag, self.lbd, self.lbd, z, z, z], dim=-1),
+                torch.stack([self.lbd, diag, self.lbd, z, z, z], dim=-1),
+                torch.stack([self.lbd, self.lbd, diag, z, z, z], dim=-1),
                 torch.stack([z, z, z, self.G, z, z], dim=-1),
                 torch.stack([z, z, z, z, self.G, z], dim=-1),
                 torch.stack([z, z, z, z, z, self.G], dim=-1),
@@ -53,9 +49,11 @@ class Isotropic:
         """Create a vectorized copy of the material for `n_elm` elements."""
         E = self.E.repeat(n_elem)
         nu = self.nu.repeat(n_elem)
-        return Isotropic(E, nu)
+        eps0 = self.eps0.repeat(n_elem)
+        return Isotropic(E, nu, eps0)
 
     def step(self, depsilon, epsilon, sigma):
+        """Perform a strain increment."""
         epsilon_new = epsilon + depsilon
         sigma_new = sigma + torch.einsum("...ij,...j->...i", self.C, depsilon)
         ddsdde = self.C

src/torchfem/solid.py
@@ -13,6 +13,7 @@ class Solid:
         self.forces = torch.zeros_like(nodes)
         self.displacements = torch.zeros_like(nodes)
         self.constraints = torch.zeros_like(nodes, dtype=bool)
+        self.ext_strain = torch.zeros(self.n_elem, 6)
         if len(elements[0]) == 4:
             self.etype = Tetra1()
         elif len(elements[0]) == 8:
@@ -52,7 +53,7 @@ class Solid:
         D5 = torch.stack([B[:, 1, :], B[:, 0, :], zeros], dim=-1).reshape(shape)
         return torch.stack([D0, D1, D2, D3, D4, D5], dim=1)
 
-    def integrate_step(self, de, ds, du):
+    def integrate_step(self, de, ds, du, dde0):
         """Perform numerical integrations for element stiffness matrix."""
         nodes = self.nodes[self.elements, :]
         du = du[self.elements, :].reshape(self.n_elem, -1)
@@ -60,17 +61,18 @@ class Solid:
         f = torch.zeros((self.n_elem, 3 * self.etype.nodes))
         epsilon = torch.zeros(self.n_elem, 6)
         sigma = torch.zeros(self.n_elem, 6)
+        N = len(self.etype.iweights())
         for w, q in zip(self.etype.iweights(), self.etype.ipoints()):
             # Compute gradient operators
             J, detJ = self.J(q, nodes)
             B = torch.matmul(torch.linalg.inv(J), self.etype.B(q))
             D = self.D(B)
             # Evaluate material response
-            dde = torch.einsum("...ij,...j->...i", D, du)
+            dde = torch.einsum("...ij,...j->...i", D, du) - dde0
             e_new, s_new, ddsdde = self.material.step(dde, de, ds)
             # Compute contribution to element strain and stress
-            epsilon[:, :] += (w * detJ)[:, None] * e_new
-            sigma[:, :] += (w * detJ)[:, None] * s_new
+            epsilon[:, :] += e_new / N
+            sigma[:, :] += s_new / N
             # Compute element internal forces
             f[:, :] += (w * detJ)[:, None] * torch.einsum("...ij,...i->...j", D, s_new)
             # Compute element stiffness matrix
@@ -113,17 +115,23 @@ class Solid:
         u = torch.zeros_like(self.nodes)
         du = torch.zeros_like(self.nodes).ravel()
 
+        # Incremental loading
         for i in range(1, len(increments)):
             # Increment size
             inc = increments[i] - increments[i - 1]
+
             # Load increment
             F_ext = inc * self.forces.ravel()
             DU = inc * self.displacements.clone().ravel()
+            DE = inc * self.ext_strain
+
+            # Newton-Raphson iterations
             for j in range(max_iter):
                 du[con] = DU[con]
+
                 # Element-wise integration
                 k, f_int, epsilon_new, sigma_new = self.integrate_step(
-                    epsilon, sigma, du.reshape((-1, 3))
+                    epsilon, sigma, du.reshape((-1, 3)), DE
                 )
 
                 # Assemble global stiffness matrix and internal force vector