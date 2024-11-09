eeb876b8296e3ada12826ab288eead99902d6f29
Nils Meyer <nils.meyer@uni-a.de>
2024-11-09 23:15:19 +0100

    Restructure basic examples.

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,23 +1,63 @@
 from testbook import testbook
 
 
-@testbook("examples/basic/planar.ipynb", execute=True)
-def test_planar_notebook(tb):
+@testbook("examples/basic/planar/cantilever.ipynb", execute=True)
+def test_planar_cantilever_notebook(tb):
     tb.execute()
 
 
-@testbook("examples/basic/shell.ipynb", execute=True)
-def test_shell_notebook(tb):
+@testbook("examples/basic/planar/fillet.ipynb", execute=True)
+def test_planar_fillet_notebook(tb):
     tb.execute()
 
 
-@testbook("examples/basic/truss.ipynb", execute=True)
-def test_truss_notebook(tb):
+@testbook("examples/basic/planar/minimal.ipynb", execute=True)
+def test_planar_minimal_notebook(tb):
     tb.execute()
 
 
-@testbook("examples/basic/solid.ipynb", execute=True)
-def test_solid_notebook(tb):
+@testbook("examples/basic/planar/plasticity.ipynb", execute=True)
+def test_planar_plasticity_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/shell/cantilever.ipynb", execute=True)
+def test_shell_cantilever_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/shell/plate.ipynb", execute=True)
+def test_shell_plate_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/truss/elasticity_2D.ipynb", execute=True)
+def test_truss_elasticity2d_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/truss/elasticity_3D.ipynb", execute=True)
+def test_truss_elasticity3d_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/truss/plasticity_2D.ipynb", execute=True)
+def test_truss_plasticity2d_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/solid/cubes.ipynb", execute=True)
+def test_solid_cubes_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/solid/inelasticity.ipynb", execute=True)
+def test_solid_inelasticity_notebook(tb):
+    tb.execute()
+
+
+@testbook("examples/basic/solid/plasticity.ipynb", execute=True)
+def test_solid_plasticity_notebook(tb):
     tb.execute()
 
 
