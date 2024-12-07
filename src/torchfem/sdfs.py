8db87f9ff6b6b713e346abf90942c8257d5b6a24
Nils Meyer <nils.meyer@uni-a.de>
2024-12-07 19:35:23 +0100

    Many more implicit features.

--- src/torchfem/sdfs.py
+++ src/torchfem/sdfs.py
@@ -4,46 +4,121 @@ import torch
 from torch import Tensor
 
 EPS = 1e-10
+torch.set_default_dtype(torch.float64)
 
 
 class SDF:
-    def __init__(self, center: Tensor = torch.zeros(3)):
+    def __init__(self, center: Tensor = torch.zeros(3), scale: Tensor = torch.ones(3)):
         self.center = center
+        self.transform = torch.diag(1 / scale)
 
     @abstractmethod
-    def f(self, points: Tensor) -> Tensor:
+    def _f(self, points: Tensor) -> Tensor:
         raise NotImplementedError
 
     @abstractmethod
-    def grad(self, points: Tensor) -> Tensor:
+    def _grad(self, points: Tensor) -> Tensor:
         raise NotImplementedError
+
+    def __and__(self, other):
+        return Intersection(self, other)
+
+    def __or__(self, other):
+        return Union(self, other)
+
+    def __sub__(self, other):
+        return Difference(self, other)
 
     def sdf(self, points: Tensor) -> Tensor:
-        return self.f(points) / (torch.norm(self.grad(points), dim=1) + EPS)
+        return self._f(points) / (torch.norm(self._grad(points), dim=1) + EPS)
+
+    def scale(self, scale: Tensor):
+        self.transform = self.transform @ torch.diag(1 / scale)
+        return self
+
+    def translate(self, center: Tensor):
+        self.center += center
+        return self
+
+    def rotate(self, axis: Tensor, angle: Tensor):
+        axis = axis / torch.norm(axis)
+        x, y, z = axis
+        c = torch.cos(angle)
+        s = torch.sin(angle)
+        t = 1 - c
+        rotation = torch.tensor(
+            [
+                [t * x * x + c, t * x * y - s * z, t * x * z + s * y],
+                [t * x * y + s * z, t * y * y + c, t * y * z - s * x],
+                [t * x * z - s * y, t * y * z + s * x, t * z * z + c],
+            ]
+        )
+        self.transform = rotation @ self.transform
+        return self
 
     def _to_xyz(self, points: Tensor) -> tuple[Tensor, Tensor, Tensor]:
-        return (
-            points[:, 0] - self.center[0],
-            points[:, 1] - self.center[1],
-            points[:, 2] - self.center[2],
-        )
+        p = (points - self.center) @ self.transform
+        return p[:, 0], p[:, 1], p[:, 2]
+
+
+class Boolean(SDF):
+    def __init__(self, sdf1: SDF, sdf2: SDF):
+        super().__init__((sdf1.center + sdf2.center) / 2)
+        self.sdf1 = sdf1
+        self.sdf2 = sdf2
+
+    def translate(self, center):
+        self.sdf1.translate(center)
+        self.sdf2.translate(center)
+        return self
+
+    def scale(self, scale):
+        self.sdf1.scale(scale)
+        self.sdf2.scale(scale)
+        return self
+
+    def rotate(self, axis, angle):
+        self.sdf1.rotate(axis, angle)
+        self.sdf2.rotate(axis, angle)
+        return self
+
+
+class Union(Boolean):
+
+    def sdf(self, points: Tensor) -> Tensor:
+        return torch.min(self.sdf1.sdf(points), self.sdf2.sdf(points))
+
+
+class Intersection(Boolean):
+
+    def sdf(self, points: Tensor) -> Tensor:
+        return torch.max(self.sdf1.sdf(points), self.sdf2.sdf(points))
+
+
+class Difference(Boolean):
+
+    def sdf(self, points: Tensor) -> Tensor:
+        return torch.max(self.sdf1.sdf(points), -self.sdf2.sdf(points))
 
 
 class Gyroid(SDF):
-    def __init__(self, center: Tensor = torch.zeros(3), scale: float = 2 * torch.pi):
-        super().__init__(center)
-        self.scale = scale
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
+    ):
+        super().__init__(center, scale)
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return (
             torch.sin(x) * torch.cos(y)
             + torch.sin(y) * torch.cos(z)
             + torch.sin(z) * torch.cos(x)
         )
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = torch.cos(x) * torch.cos(y) - torch.sin(z) * torch.cos(x)
         grad_y = torch.cos(y) * torch.cos(z) - torch.sin(x) * torch.cos(y)
         grad_z = torch.cos(z) * torch.cos(x) - torch.sin(y) * torch.cos(z)
@@ -54,19 +129,18 @@ class SchwarzP(SDF):
     def __init__(
         self,
         center: Tensor = torch.zeros(3),
-        scale: float = 2 * torch.pi,
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
         c: float = 0.0,
     ):
-        super().__init__(center)
-        self.scale = scale
+        super().__init__(center, scale)
         self.c = c
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return torch.cos(x) + torch.cos(y) + torch.cos(z) - self.c
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = -torch.sin(x)
         grad_y = -torch.sin(y)
         grad_z = -torch.sin(z)
@@ -74,12 +148,15 @@ class SchwarzP(SDF):
 
 
 class Diamond(SDF):
-    def __init__(self, center: Tensor = torch.zeros(3), scale: float = 2 * torch.pi):
-        super().__init__(center)
-        self.scale = scale
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
+    ):
+        super().__init__(center, scale)
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return (
             torch.sin(x) * torch.sin(y) * torch.sin(z)
             + torch.sin(x) * torch.cos(y) * torch.cos(z)
@@ -87,8 +164,8 @@ class Diamond(SDF):
             + torch.cos(x) * torch.cos(y) * torch.sin(z)
         )
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = (
             torch.cos(x) * torch.sin(y) * torch.sin(z)
             + torch.cos(x) * torch.cos(y) * torch.cos(z)
@@ -111,12 +188,15 @@ class Diamond(SDF):
 
 
 class Lidinoid(SDF):
-    def __init__(self, center: Tensor = torch.zeros(3), scale: float = 2 * torch.pi):
-        super().__init__(center)
-        self.scale = scale
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
+    ):
+        super().__init__(center, scale)
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return (
             torch.sin(2 * x) * torch.cos(y) * torch.sin(z)
             + torch.sin(2 * y) * torch.cos(z) * torch.sin(x)
@@ -127,8 +207,8 @@ class Lidinoid(SDF):
             + 0.3
         )
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = (
             2 * torch.cos(2 * x) * torch.cos(y) * torch.sin(z)
             + torch.cos(x) * torch.sin(2 * z) * torch.sin(y)
@@ -151,12 +231,15 @@ class Lidinoid(SDF):
 
 
 class SplitP(SDF):
-    def __init__(self, center: Tensor = torch.zeros(3), scale: float = 2 * torch.pi):
-        super().__init__(center)
-        self.scale = scale
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
+    ):
+        super().__init__(center, scale)
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return (
             1.1
             * (
@@ -173,8 +256,8 @@ class SplitP(SDF):
             - 0.4 * (torch.cos(2 * x) + torch.cos(2 * y) + torch.cos(2 * z))
         )
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = (
             2.2 * torch.cos(2 * x) * torch.sin(z) * torch.cos(y)
             + torch.cos(x) * torch.sin(2 * y) * torch.cos(z)
@@ -209,18 +292,21 @@ class SplitP(SDF):
 
 
 class Neovius(SDF):
-    def __init__(self, center: Tensor = torch.zeros(3), scale: float = 2 * torch.pi):
-        super().__init__(center)
-        self.scale = scale
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        scale: Tensor = 1 / (2 * torch.pi) * torch.ones(3),
+    ):
+        super().__init__(center, scale)
 
-    def f(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         return 3 * (torch.cos(x) + torch.cos(y) + torch.cos(z)) + 4 * torch.cos(
             x
         ) * torch.cos(y) * torch.cos(z)
 
-    def grad(self, points: Tensor) -> Tensor:
-        x, y, z = self._to_xyz(self.scale * points)
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
         grad_x = -3 * torch.sin(x) - 4 * torch.sin(x) * torch.cos(y) * torch.cos(z)
         grad_y = -3 * torch.sin(y) - 4 * torch.cos(x) * torch.sin(y) * torch.cos(z)
         grad_z = -3 * torch.sin(z) - 4 * torch.cos(x) * torch.cos(y) * torch.sin(z)
@@ -232,11 +318,11 @@ class Sphere(SDF):
         super().__init__(center)
         self.radius = radius
 
-    def f(self, points: Tensor) -> Tensor:
+    def _f(self, points: Tensor) -> Tensor:
         x, y, z = self._to_xyz(points)
         return torch.sqrt(x**2 + y**2 + z**2) - self.radius
 
-    def grad(self, points: Tensor) -> Tensor:
+    def _grad(self, points: Tensor) -> Tensor:
         x, y, z = self._to_xyz(points)
         grad_x = x / (torch.sqrt(x**2 + y**2 + z**2) + EPS)
         grad_y = y / (torch.sqrt(x**2 + y**2 + z**2) + EPS)
@@ -244,27 +330,106 @@ class Sphere(SDF):
         return torch.stack([grad_x, grad_y, grad_z], dim=1)
 
 
-class Cube(SDF):
+class Torus(SDF):
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        radius: float = 1.0,
+        tube_radius: float = 0.5,
+    ):
+        super().__init__(center)
+        self.radius = radius
+        self.tube_radius = tube_radius
+
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
+        q = torch.sqrt(x**2 + z**2) - self.radius
+        return torch.sqrt(q**2 + y**2) - self.tube_radius
+
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
+        q = torch.sqrt(x**2 + z**2) - self.radius
+        grad_x = x * (q / torch.sqrt(q**2 + y**2))
+        grad_y = y / torch.sqrt(q**2 + y**2)
+        grad_z = z * (q / torch.sqrt(q**2 + y**2))
+        return torch.stack([grad_x, grad_y, grad_z], dim=1)
+
+
+class Box(SDF):
     def __init__(self, center: Tensor = torch.zeros(3), size: Tensor = torch.ones(3)):
         super().__init__(center)
         self.size = size
 
-    def f(self, points: Tensor) -> Tensor:
+    def _f(self, points: Tensor) -> Tensor:
         x, y, z = self._to_xyz(points)
         return torch.max(
             torch.stack(
                 [
-                    torch.abs(x) - self.size[0],
-                    torch.abs(y) - self.size[1],
-                    torch.abs(z) - self.size[2],
+                    torch.abs(x) - self.size[0] / 2,
+                    torch.abs(y) - self.size[1] / 2,
+                    torch.abs(z) - self.size[2] / 2,
                 ],
             ),
             dim=0,
         ).values
 
-    def grad(self, points: Tensor) -> Tensor:
+    def _grad(self, points: Tensor) -> Tensor:
         x, y, z = self._to_xyz(points)
         grad_x = torch.sign(x)
         grad_y = torch.sign(y)
         grad_z = torch.sign(z)
         return torch.stack([grad_x, grad_y, grad_z], dim=1)
+
+
+class Cylinder(SDF):
+    def __init__(
+        self,
+        center: Tensor = torch.zeros(3),
+        radius: float = 1.0,
+        height: float = 1.0,
+    ):
+        super().__init__(center)
+        self.radius = radius
+        self.height = height
+
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
+        return torch.max(
+            torch.sqrt(x**2 + z**2) - self.radius,
+            torch.abs(y) - self.height / 2,
+        )
+
+    def _grad(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
+        grad_x = x / (torch.sqrt(x**2 + z**2) + EPS)
+        grad_y = torch.sign(y)
+        grad_z = z / (torch.sqrt(x**2 + z**2) + EPS)
+        return torch.stack([grad_x, grad_y, grad_z], dim=1)
+
+
+class Plane(SDF):
+    def __init__(
+        self, center: Tensor = torch.zeros(3), normal: Tensor = torch.tensor([0, 0, 1])
+    ):
+        super().__init__(center)
+        self.normal = normal / torch.norm(normal)
+
+    def _f(self, points: Tensor) -> Tensor:
+        x, y, z = self._to_xyz(points)
+        return x * self.normal[0] + y * self.normal[1] + z * self.normal[2]
+
+    def _grad(self, points: Tensor) -> Tensor:
+        return self.normal * torch.ones_like(points)
+
+
+class Shell(SDF):
+    def __init__(self, other: SDF, thickness: float):
+        super().__init__(other.center)
+        self.other = other
+        self.thickness = thickness
+
+    def _f(self, points: Tensor) -> Tensor:
+        return torch.abs(self.other.sdf(points)) - 0.5 * self.thickness
+
+    def _grad(self, points: Tensor) -> Tensor:
+        return self.other._grad(points)
