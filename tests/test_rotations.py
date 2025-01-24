124f6ca60334ee678adcb553b8070c00568dda8a
Nils Meyer <nils.meyer@uni-a.de>
2025-01-24 14:33:51 +0100

    Fix test.

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -4,8 +4,6 @@ import torch
 from torchfem.materials import OrthotropicElasticity3D, OrthotropicElasticityPlaneStress
 from torchfem.rotations import axis_rotation, euler_rotation, planar_rotation
 
-torch.set_default_dtype(torch.float64)
-
 mat_2D = OrthotropicElasticityPlaneStress(1.0, 2.0, 0.3, 1.0)
 mat_2D_rot = OrthotropicElasticityPlaneStress(2.0, 1.0, 0.6, 1.0)
 
@@ -36,4 +34,4 @@ def test_orthogonality(R):
 )
 def test_stiffness_rotation(mat, mat_rot, R):
     rotated_material = mat.rotate(R)
-    assert torch.allclose(rotated_material.C, mat_rot.C)
+    assert torch.allclose(rotated_material.C, mat_rot.C, atol=1e-6)
