307a0e63cda91b81fb3ac63743043e3878b89b46
Krauß <be9399@partner.kit.edu>
2025-10-17 17:10:39 +0200

    added functions and example for 2D stationary heat problem

--- src/torchfem/planar.py
+++ src/torchfem/planar.py
@@ -5,7 +5,7 @@ from matplotlib.collections import PolyCollection
 from matplotlib.tri import Triangulation
 from torch import Tensor
 
-from .base import FEM
+from .base import FEM, FEMHEat
 from .elements import Quad1, Quad2, Tria1, Tria2
 from .materials import Material
 
@@ -70,7 +70,7 @@ class Planar(FEM):
 
     def compute_f(self, detJ: Tensor, B: Tensor, S: Tensor):
         """Element internal force vector."""
-        return torch.einsum("...,...,...ik,...ij->...kj", self.thickness, detJ, B, S)
+        return torch.einsum("...,...,...iI,...Ai->...IA", self.thickness, detJ, B, S)
 
     @torch.no_grad()
     def plot(
@@ -210,6 +210,163 @@ class Planar(FEM):
                 headaxislength=0,
                 headwidth=0,
                 width=0.005,
+            )
+
+        if title:
+            ax.set_title(title)
+
+        ax.set_aspect("equal", adjustable="box")
+        if not axes:
+            ax.set_axis_off()
+
+
+class PlanarHeat(FEMHEat, Planar):
+    n_dof_per_node = 1
+
+    def __init__(self, nodes: Tensor, elements: Tensor, material: Material):
+        super().__init__(nodes, elements, material)
+        self.ext_strain = torch.zeros(self.n_elem, self.n_dof_per_node, self.n_dim)
+
+    @torch.no_grad()
+    def plot(
+        self,
+        u: float | Tensor = 0.0,
+        node_property: Tensor | None = None,
+        element_property: Tensor | None = None,
+        orientation: Tensor | None = None,
+        element_vector_field: Tensor | None = None,
+        node_labels: bool = False,
+        node_markers: bool = False,
+        axes: bool = False,
+        bcs: bool = True,
+        color: str = "black",
+        alpha: float = 1.0,
+        linewidth: float = 1.0,
+        figsize: tuple[float, float] = (8.0, 6.0),
+        colorbar: bool = False,
+        vmin: float | None = None,
+        vmax: float | None = None,
+        title: str | None = None,
+        ax: Axes | None = None,
+        **kwargs,
+    ):
+        cmap = kwargs.get("cmap", "viridis")
+
+        # helper functions for quiver plots
+        def plot_element_vector_field(vectors: Tensor, ax: Axes, **kwargs):
+            centers = pos[self.elements, :].mean(dim=1)
+            ax.quiver(
+                centers[:, 0],
+                centers[:, 1],
+                vectors[:, 0],
+                vectors[:, 1],
+                torch.linalg.norm(vectors, dim=1),
+                pivot="middle",
+                cmap=cmap,
+            )
+
+        # Compute deformed positions
+        pos = self.nodes
+
+        # Bounding box
+        size = torch.linalg.norm(pos.max() - pos.min())
+
+        # Set figure size
+        if ax is None:
+            _, ax = plt.subplots(figsize=figsize)
+
+        # Color surface with interpolated nodal properties (if provided)
+        if node_property is not None:
+            if isinstance(self.etype, (Quad1, Quad2)):
+                triangles = []
+                for e in self.elements:
+                    triangles.append([e[0], e[1], e[2]])
+                    triangles.append([e[2], e[3], e[0]])
+            else:
+                triangles = self.elements[:, :3].tolist()
+            triangulation = Triangulation(pos[:, 0], pos[:, 1], triangles)
+            tri = ax.tricontourf(
+                triangulation,
+                node_property,
+                cmap=cmap,
+                levels=100,
+                alpha=alpha,
+                vmin=vmin,
+                vmax=vmax,
+            )
+            if colorbar:
+                plt.colorbar(tri, ax=ax)
+
+        # Color surface with element properties (if provided)
+        if element_property is not None:
+            if element_property.shape == (self.n_elem,):
+                if isinstance(self.etype, Tria2):
+                    verts = pos[self.elements[:, :3]]
+                elif isinstance(self.etype, Quad2):
+                    verts = pos[self.elements[:, :4]]
+                else:
+                    verts = pos[self.elements]
+                pc = PolyCollection([v for v in verts.numpy()], cmap=cmap)
+                pc.set_array(element_property)
+                ax.add_collection(pc)
+                if colorbar:
+                    plt.colorbar(pc, ax=ax)
+                    pc.set_clim(vmin=vmin, vmax=vmax)
+            else:
+                raise ValueError(
+                    "Element property must be a scalar field with the same number of elements as the mesh."
+                )
+
+        if element_vector_field is not None:
+            # print("here")
+            plot_element_vector_field(element_vector_field, ax, **kwargs)
+
+        # Nodes
+        if node_markers:
+            ax.scatter(pos[:, 0], pos[:, 1], color=color, marker="o")
+            if node_labels:
+                for i, node in enumerate(pos):
+                    ax.annotate(
+                        str(i),
+                        (node[0].item() + 0.01, node[1].item() + 0.01),
+                        color=color,
+                    )
+
+        # Elements
+        for element in self.elements:
+            if isinstance(self.etype, Tria2):
+                element = element[:3]
+            if isinstance(self.etype, Quad2):
+                element = element[:4]
+            x1 = [pos[node, 0] for node in element] + [pos[element[0], 0]]
+            x2 = [pos[node, 1] for node in element] + [pos[element[0], 1]]
+            ax.plot(x1, x2, color=color, linewidth=linewidth)
+
+        # Constraints
+        if bcs:
+            for i, constraint in enumerate(self.constraints):
+                x = float(pos[i][0])
+                y = float(pos[i][1])
+                if constraint[0]:
+                    ax.plot(x, y, "s", color="gray")
+
+        # Material orientations
+        if orientation is not None:
+            centers = pos[self.elements, :].mean(dim=1)
+            dir = torch.stack(
+                [
+                    torch.cos(orientation),
+                    -torch.sin(orientation),
+                    torch.zeros_like(orientation),
+                ]
+            ).T
+            ax.quiver(
+                centers[:, 0],
+                centers[:, 1],
+                dir[:, 0],
+                dir[:, 1],
+                pivot="middle",
+                zorder=-1,
             )
 
         if title:
