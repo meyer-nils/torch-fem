1eeec96cb4098e0a88863be2381e891bda55971a
Krauß <be9399@partner.kit.edu>
2025-10-20 17:21:15 +0200

    isoparametric coordinates on element class are now wrapped as property as well

--- src/torchfem/elements.py
+++ src/torchfem/elements.py
@@ -13,7 +13,6 @@ ELEMENT_REGISTRY: list[type["Element"]] = []
 
 
 class Element(ABC):
-    iso_coords: Tensor
     iso_volume: float
     iso_dim: int
     nodes: int
@@ -36,6 +35,11 @@ class Element(ABC):
 
     @classproperty
     @abstractmethod
+    def iso_coords(cls) -> Tensor:
+        pass
+
+    @classproperty
+    @abstractmethod
     def ipoints(cls) -> Tensor:
         pass
 
@@ -46,10 +50,13 @@ class Element(ABC):
 
 
 class Bar1(Element):
-    iso_coords = torch.tensor([[-1.0], [1.0]])
     iso_volume = 2.0
     iso_dim = 1
     nodes = 2
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor([[-1.0], [1.0]])
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -75,8 +82,11 @@ class Bar1(Element):
 
 
 class Bar2(Bar1):
-    iso_coords = torch.tensor([[-1.0], [1.0], [0.0]])
     nodes = 3
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor([[-1.0], [1.0], [0.0]])
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -111,10 +121,13 @@ class Bar2(Bar1):
 
 
 class Tria1(Element):
-    iso_coords = torch.tensor([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
     iso_volume = 0.5
     iso_dim = 2
     nodes = 3
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -141,10 +154,20 @@ class Tria1(Element):
 
 
 class Tria2(Tria1):
-    iso_coords = torch.tensor(
-        [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [0.5, 0.0], [0.5, 0.5], [0.0, 0.5]]
-    )
     nodes = 6
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [
+                [0.0, 0.0],
+                [1.0, 0.0],
+                [0.0, 1.0],
+                [0.5, 0.0],
+                [0.5, 0.5],
+                [0.0, 0.5],
+            ]
+        )
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -197,10 +220,13 @@ class Tria2(Tria1):
 
 
 class Quad1(Element):
-    iso_coords = torch.tensor([[-1.0, -1.0], [1.0, -1.0], [1.0, 1.0], [-1.0, 1.0]])
     iso_volume = 4.0
     iso_dim = 2
     nodes = 4
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor([[-1.0, -1.0], [1.0, -1.0], [1.0, 1.0], [-1.0, 1.0]])
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -252,21 +278,24 @@ class Quad1(Element):
 
 
 class Quad2(Quad1):
-    iso_coords = torch.tensor(
-        [
-            # 4 corner nodes
-            [-1.0, -1.0],
-            [1.0, -1.0],
-            [1.0, 1.0],
-            [-1.0, 1.0],
-            # 4 midside nodes
-            [0.0, -1.0],
-            [1.0, 0.0],
-            [0.0, 1.0],
-            [-1.0, 0.0],
-        ]
-    )
     nodes = 8
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [
+                # 4 corner nodes
+                [-1.0, -1.0],
+                [1.0, -1.0],
+                [1.0, 1.0],
+                [-1.0, 1.0],
+                # 4 midside nodes
+                [0.0, -1.0],
+                [1.0, 0.0],
+                [0.0, 1.0],
+                [-1.0, 0.0],
+            ]
+        )
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -330,12 +359,15 @@ class Quad2(Quad1):
 
 
 class Tetra1(Element):
-    iso_coords = torch.tensor(
-        [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-    )
     iso_volume = 1.0 / 6.0
     iso_dim = 3
     nodes = 4
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+        )
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -367,23 +399,26 @@ class Tetra1(Element):
 
 
 class Tetra2(Tetra1):
-    iso_coords = torch.tensor(
-        [
-            # 4 corner nodes
-            [0.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0],
-            # 6 midside nodes
-            [0.5, 0.0, 0.0],
-            [0.5, 0.5, 0.0],
-            [0.0, 0.5, 0.0],
-            [0.0, 0.0, 0.5],
-            [0.5, 0.0, 0.5],
-            [0.0, 0.5, 0.5],
-        ]
-    )
     nodes = 10
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [
+                # 4 corner nodes
+                [0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0],
+                # 6 midside nodes
+                [0.5, 0.0, 0.0],
+                [0.5, 0.5, 0.0],
+                [0.0, 0.5, 0.0],
+                [0.0, 0.0, 0.5],
+                [0.5, 0.0, 0.5],
+                [0.0, 0.5, 0.5],
+            ]
+        )
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
@@ -472,21 +507,24 @@ class Tetra2(Tetra1):
 
 
 class Hexa1(Element):
-    iso_coords = torch.tensor(
-        [
-            [-1.0, -1.0, -1.0],
-            [1.0, -1.0, -1.0],
-            [1.0, 1.0, -1.0],
-            [-1.0, 1.0, -1.0],
-            [-1.0, -1.0, 1.0],
-            [1.0, -1.0, 1.0],
-            [1.0, 1.0, 1.0],
-            [-1.0, 1.0, 1.0],
-        ]
-    )
     iso_volume = 8.0
     iso_dim = 3
     nodes = 8
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [
+                [-1.0, -1.0, -1.0],
+                [1.0, -1.0, -1.0],
+                [1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0],
+                [-1.0, -1.0, 1.0],
+                [1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0],
+                [-1.0, 1.0, 1.0],
+            ]
+        )
 
     @classproperty
     def iweights(cls) -> Tensor:
@@ -564,34 +602,37 @@ class Hexa1(Element):
 
 
 class Hexa2(Hexa1):
-    iso_coords = torch.tensor(
-        [
-            # 8 corner nodes
-            [-1.0, -1.0, -1.0],
-            [1.0, -1.0, -1.0],
-            [1.0, 1.0, -1.0],
-            [-1.0, 1.0, -1.0],
-            [-1.0, -1.0, 1.0],
-            [1.0, -1.0, 1.0],
-            [1.0, 1.0, 1.0],
-            [-1.0, 1.0, 1.0],
-            # 12 midside nodes
-            [0.0, -1.0, -1.0],
-            [1.0, 0.0, -1.0],
-            [0.0, 1.0, -1.0],
-            [-1.0, 0.0, -1.0],
-            [0.0, -1.0, 1.0],
-            [1.0, 0.0, 1.0],
-            [0.0, 1.0, 1.0],
-            [-1.0, 0.0, 1.0],
-            [-1.0, -1.0, 0.0],
-            [1.0, -1.0, 0.0],
-            [1.0, 1.0, 0.0],
-            [-1.0, 1.0, 0.0],
-        ]
-    )
     iso_dim = 3
     nodes = 20
+
+    @classproperty
+    def iso_coords(cls) -> Tensor:
+        return torch.tensor(
+            [
+                # 8 corner nodes
+                [-1.0, -1.0, -1.0],
+                [1.0, -1.0, -1.0],
+                [1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0],
+                [-1.0, -1.0, 1.0],
+                [1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0],
+                [-1.0, 1.0, 1.0],
+                # 12 midside nodes
+                [0.0, -1.0, -1.0],
+                [1.0, 0.0, -1.0],
+                [0.0, 1.0, -1.0],
+                [-1.0, 0.0, -1.0],
+                [0.0, -1.0, 1.0],
+                [1.0, 0.0, 1.0],
+                [0.0, 1.0, 1.0],
+                [-1.0, 0.0, 1.0],
+                [-1.0, -1.0, 0.0],
+                [1.0, -1.0, 0.0],
+                [1.0, 1.0, 0.0],
+                [-1.0, 1.0, 0.0],
+            ]
+        )
 
     @classmethod
     def N(cls, xi: Tensor) -> Tensor:
