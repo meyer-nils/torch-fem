dbbb02337a2d8bc49d74ee94e36f6f66778ac4c2
Nils Meyer <nils.meyer@uni-a.de>
2026-03-25 17:13:21 +0100

    Swap index for drilling stiffness in shells. The current version is potentially dangerous if outer integration index i should be used again.

--- src/torchfem/shell.py
+++ src/torchfem/shell.py
@@ -365,8 +365,8 @@ class Shell(Mechanics):
 
             # Element drilling stiffness
             kd = torch.zeros_like(km)
-            for i in range(self.etype.nodes):
-                kd[:, i * self.n_dof_per_node - 1, i * self.n_dof_per_node - 1] = (
+            for z in range(self.etype.nodes):
+                kd[:, z * self.n_dof_per_node - 1, z * self.n_dof_per_node - 1] = (
                     self.drill_penalty
                 )
 
